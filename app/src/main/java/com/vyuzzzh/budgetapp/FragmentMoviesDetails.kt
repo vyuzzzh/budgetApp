2d4b8f1200cbb80bea87e72327348f51bffd266e
viuzhanin_m <viuzhanin_m@tass.ru>
2020-12-02 02:43:47 +0300

    feat: создать контроллеры для фрагментов детали и списка

---
+++ app/src/main/java/com/vyuzzzh/budgetapp/FragmentMoviesDetails.kt
@@ -0,0 +1,11 @@
+package com.vyuzzzh.budgetapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FragmentMoviesDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_movies_details)
+    }
+}
