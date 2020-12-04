f8dead307f1bdd754801d51c1ef3352a4f4e6e23
viuzhanin_m <viuzhanin_m@tass.ru>
2020-12-04 03:07:09 +0300

    feat: реализовать переход от фрагмента детали к листу

--- app/src/main/java/com/vyuzzzh/budgetapp/FragmentMoviesDetails.kt
+++ app/src/main/java/com/vyuzzzh/budgetapp/FragmentMoviesDetails.kt
@@ -1,21 +1,48 @@
 package com.vyuzzzh.budgetapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class FragmentMoviesDetails : Fragment() {
+    private var fragmentClick: MoviesDetailsClick? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_movies_details, container, false)
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.back_button)?.setOnClickListener {
+            fragmentClick?.backToMoviesList()
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is MoviesDetailsClick) {
+            fragmentClick = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        fragmentClick = null
+    }
+
     companion object {
         fun newInstance(): FragmentMoviesDetails {
             return FragmentMoviesDetails()
         }
+    }
+
+    interface MoviesDetailsClick {
+        fun backToMoviesList()
     }
 }
--- app/src/main/java/com/vyuzzzh/budgetapp/MainActivity.kt
+++ app/src/main/java/com/vyuzzzh/budgetapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.vyuzzzh.budgetapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), FragmentMoviesList.TransactionsFragmentClick {
+class MainActivity :
+        AppCompatActivity(),
+        FragmentMoviesList.TransactionsFragmentClick,
+        FragmentMoviesDetails.MoviesDetailsClick {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +22,11 @@ class MainActivity : AppCompatActivity(), FragmentMoviesList.TransactionsFragmen
         supportFragmentManager.beginTransaction()
             .add(R.id.main_container, FragmentMoviesDetails.newInstance())
             .commit()
+    }
+
+    override fun backToMoviesList() {
+        supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, FragmentMoviesList.newInstance())
+                .commit()
     }
 }
